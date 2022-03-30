07c023d5e00aef0c7b582ea3909c8b917a68aa16
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-29 21:37:36 -0600

    Add route to update featured images

--- app/controllers/featured_work_controller.rb
+++ app/controllers/featured_work_controller.rb
@@ -1,11 +1,27 @@
 class FeaturedWorkController < ApplicationController
 
-  def index
+  def index # Returns an array of ALL featured image urls
     if valid_api_key?(params[:api_key])
       featured_images = FeaturedImage.all
       render json: FeaturedImageSerializer.index(featured_images)
     else
-      render json: ErrorSerializer.main(["api_key param is either missing or invalid"])
+      render json: ErrorSerializer.main(["api_key param is either missing or invalid"]), status: 401
+    end
+  end
+
+  def update # Destroys all featured images, then updates them
+    if valid_api_key?(params[:api_key])
+      FeaturedImage.destroy_all
+
+      params[:urls] ||= []
+      params[:urls].each do |url|
+        FeaturedImage.create(url: url)
+      end
+
+      featured_images = FeaturedImage.all
+      render json: FeaturedImageSerializer.index(featured_images)
+    else
+      render json: ErrorSerializer.main(["api_key param is either missing or invalid"]), status: 401
     end
   end
 
