19014653099d0e269ff0985449df77fd68234671
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-30 17:20:42 -0600

    Change about me search cells

--- app/controllers/about_me_controller.rb
+++ app/controllers/about_me_controller.rb
@@ -3,8 +3,8 @@ class AboutMeController < ApplicationController
   def index
     if valid_api_key?(params[:api_key])
       spreadsheet_id = "1dezRYzkCZ8VrfsbO2EKVoF9D_hIHsKAF_BuI7b83phA"
-      selection1 = "B22"
-      selection2 = "B22"
+      selection1 = "B2"
+      selection2 = "B2"
       # binding.pry
       about_me = GoogleSheetsFacade.get_sheet_data(spreadsheet_id, selection1, selection2).first[0]
       about_me = "" if about_me.nil?
